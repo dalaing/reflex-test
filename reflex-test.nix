711829d77d95271506b00bca9bb9d66da8b76a0c
Dave Laing <dave.laing.80@gmail.com>
2018-05-30 09:41:37 +1000

    Adds lens

--- reflex-test.nix
+++ reflex-test.nix
@@ -1,4 +1,4 @@
-{ mkDerivation, base, containers, ghcjs-dom, jsaddle-dom, mtl
+{ mkDerivation, base, containers, ghcjs-dom, jsaddle-dom, lens, mtl
 , reflex, reflex-basic-host, reflex-dom, stdenv, stm, text
 , transformers
 }:
@@ -7,8 +7,8 @@ mkDerivation {
   version = "0.1.0.0";
   src = ./.;
   libraryHaskellDepends = [
-    base containers ghcjs-dom jsaddle-dom mtl reflex reflex-basic-host
-    reflex-dom stm text transformers
+    base containers ghcjs-dom jsaddle-dom lens mtl reflex
+    reflex-basic-host reflex-dom stm text transformers
   ];
   license = stdenv.lib.licenses.bsd3;
 }
